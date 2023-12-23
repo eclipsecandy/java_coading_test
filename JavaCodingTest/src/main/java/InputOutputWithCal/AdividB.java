b5ac5b6987135120713cc39fefc53337f6d0131f
eclipsecandy <kh.rang73@gmail.com>
2023-12-24 00:48:54 +0900

    얏호

---
+++ JavaCodingTest/src/main/java/InputOutputWithCal/AdividB.java
@@ -0,0 +1,12 @@
+package InputOutputWithCal;
+
+import java.util.Scanner;
+
+public class AdividB {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println((double)A / B);
+    }
+}
