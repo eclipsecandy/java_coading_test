cc3fe8bd3e4bd80dcfe36c3a0b2912eced9c52f6
eclipsecandy <kh.rang73@gmail.com>
2023-12-26 01:22:16 +0900

    두번째 성공

--- JavaCodingTest/src/main/java/OneDimenArray/LessThanX.java
+++ JavaCodingTest/src/main/java/OneDimenArray/LessThanX.java
@@ -7,8 +7,12 @@ public class LessThanX {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int X = sc.nextInt();
+        int[] A = new int[N];
+        StringBuilder sb = new StringBuilder();
         for(int i = 0; i < N; i++) {
-            sc
+            A[i] = sc.nextInt();
+            if(A[i] < X) sb.append(A[i] + " ");
         }
+        System.out.println(sb.toString());
     }
 }
