b3c5f7f4eb8947593beacb0a17b0ee0bbfce2c1f
eclipsecandy <kh.rang73@gmail.com>
2023-12-27 03:08:43 +0900

    코드줄이기

--- JavaCodingTest/src/main/java/OneDimenArray/FakeAverage.java
+++ JavaCodingTest/src/main/java/OneDimenArray/FakeAverage.java
@@ -8,22 +8,34 @@ public class FakeAverage {
         1546 평균
         기말고사 점수 최대값을 대비하여 새로운 평균값 구하기
          */
+        /*
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         int[] arr = new int[N];
         int max = 0;
         for(int i = 0; i < arr.length; i++) {
             arr[i] = sc.nextInt();
-            if(arr[i] > max) {
-                max = arr[i];
-            }
+            if(arr[i] > max) max = arr[i];
         }
         double sum = 0.0;
-        double[] dArr = new double[arr.length];
+        double[] dArr = new double[N];
         for(int i = 0; i < arr.length; i++) {
             dArr[i] = (double)arr[i] / max * 100;
             sum += dArr[i];
         }
         System.out.println(sum / arr.length);
+        */
+        // 코드 줄이기
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] arr = new int[N];
+        int max = 0;
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+            if(arr[i] > max) max = arr[i];
+            sum += arr[i];
+        }
+        System.out.println((double)sum / N / max * 100);
     }
 }
