776b393967b66f7f82feb5f7c5ebdef379bfcf41
eclipsecandy <kh.rang73@gmail.com>
2023-12-27 01:37:48 +0900

    어 중첩 어려움

--- JavaCodingTest/src/main/java/OneDimenArray/Remainder.java
+++ JavaCodingTest/src/main/java/OneDimenArray/Remainder.java
@@ -1,13 +1,28 @@
 package OneDimenArray;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Remainder {
     public static void main(String[] args) {
         /*
         3052 나머지
         10개의 숫자를 42로 나눈 나머지의 총 개수
          */
-        for(int i = 0; i < 10; i++) {
-
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int sum = arr.length;
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt() % 42;
         }
+        for(int i = 0; i < arr.length; i++) {
+            for(int j  = i + 1; j < arr.length; j++) {
+                if(arr[i] == arr[j]) {
+                    sum--;
+                    break;
+                }
+            }
+        }
+        System.out.println(sum);
     }
 }
