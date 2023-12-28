fb48bc24961cf717db4e2f69fb3c8fd6161f5f75
eclipsecandy <kh.rang73@gmail.com>
2023-12-29 01:35:58 +0900

    훗 int랑 char는 서로 형변환이 되지

---
+++ JavaCodingTest/src/main/java/String/AsciiCode.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.util.Scanner;
+
+public class AsciiCode {
+    public static void main(String[] args) {
+        /*
+        11654 아스키 코드
+        소문자 대문자 숫자를 아스키코드값으로 출력
+         */
+        System.out.println((int)new Scanner(System.in).nextLine().charAt(0));
+    }
+}
