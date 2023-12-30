4e8c6c776d9c220b1a9c745fb46ebfcc7080ec45
eclipsecandy <kh.rang73@gmail.com>
2023-12-31 04:36:21 +0900

    새싹이다 " \ 주의하는문제

---
+++ JavaCodingTest/src/main/java/Deepen1/A_Sprout.java
@@ -0,0 +1,15 @@
+package Deepen1;
+
+public class A_Sprout {
+    public static void main(String[] args) {
+        // 25083 새싹
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\n");
+        sb.append("r`-_   ,'  ,/\n");
+        sb.append(" \\. \". L_r'\n");
+        sb.append("   `~\\/\n");
+        sb.append("      |\n");
+        sb.append("      |");
+        System.out.println(sb.toString());
+    }
+}
