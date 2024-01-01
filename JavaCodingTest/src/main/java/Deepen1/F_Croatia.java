14d15dd681ef6436575cced338558fb344d7d309
eclipsecandy <kh.rang73@gmail.com>
2024-01-02 01:44:29 +0900

    음 왜캐 다 어려운거임

---
+++ JavaCodingTest/src/main/java/Deepen1/F_Croatia.java
@@ -0,0 +1,17 @@
+package Deepen1;
+
+import java.util.Scanner;
+
+public class F_Croatia {
+    public static void main(String[] args) {
+        /*
+        2941 크로아티아 알파벳
+        문자열이 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력
+         */
+        String str = new Scanner(System.in).nextLine();
+        String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        
+
+        System.out.println(str.contains("abc"));
+    }
+}
