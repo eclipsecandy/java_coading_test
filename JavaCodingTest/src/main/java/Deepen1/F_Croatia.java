bf44d4bada4377d1ec383963b597e5ec4215208d
eclipsecandy <kh.rang73@gmail.com>
2024-01-08 23:55:49 +0900

    크로아티아로 넘어감

--- JavaCodingTest/src/main/java/Deepen1/F_Croatia.java
+++ JavaCodingTest/src/main/java/Deepen1/F_Croatia.java
@@ -9,7 +9,9 @@ public class F_Croatia {
         문자열이 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력
          */
         String str = new Scanner(System.in).nextLine();
+        int sum = 0;
         String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        
 
 
         System.out.println(str.contains("abc"));
