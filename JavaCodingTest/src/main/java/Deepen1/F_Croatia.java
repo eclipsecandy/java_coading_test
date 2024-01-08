e7b0ca9ce85a2135a39423244348bb65efdd2c17
eclipsecandy <kh.rang73@gmail.com>
2024-01-09 01:22:31 +0900

    이게 틀릴리가 없지

--- JavaCodingTest/src/main/java/Deepen1/F_Croatia.java
+++ JavaCodingTest/src/main/java/Deepen1/F_Croatia.java
@@ -9,16 +9,26 @@ public class F_Croatia {
         문자열이 몇 개의 크로아티아 알파벳으로 이루어져 있는지 출력
          */
         String str = new Scanner(System.in).nextLine();
-        int sum = 0;
         String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+        /*
+        int sum = 0;
+        int length = str.length();
         for(int i = 0; i < arr.length; i++){
             if(str.contains(arr[i])){
-                str.substring(str.indexOf(arr[i]), 3);
-                System.out.println(str.indexOf(arr[i]));
+                if(arr[i].equals("z=") && str.indexOf(arr[i]) == str.indexOf("dz=") + 1){
+                    continue;
+                }
+                sum++;
+                length -= arr[i].length();
             }
         }
-
-
-        System.out.println(str.contains("abc"));
+        System.out.println(sum + length);
+         */
+        for(int i = 0; i < arr.length; i++){
+            if(str.contains(arr[i])){
+                str = str.replaceAll(arr[i], "!");
+            }
+        }
+        System.out.println(str.length());
     }
 }
