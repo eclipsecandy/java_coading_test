8905c273bbd2bd52ab80a583c0e6c0a80f7ef7d9
eclipsecandy <kh.rang73@gmail.com>
2024-01-09 00:03:07 +0900

    진행중

--- JavaCodingTest/src/main/java/Deepen1/F_Croatia.java
+++ JavaCodingTest/src/main/java/Deepen1/F_Croatia.java
@@ -11,7 +11,12 @@ public class F_Croatia {
         String str = new Scanner(System.in).nextLine();
         int sum = 0;
         String[] arr = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
-        
+        for(int i = 0; i < arr.length; i++){
+            if(str.contains(arr[i])){
+                str.substring(str.indexOf(arr[i]), 3);
+                System.out.println(str.indexOf(arr[i]));
+            }
+        }
 
 
         System.out.println(str.contains("abc"));
